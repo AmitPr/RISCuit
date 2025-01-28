4c3ef5c61953ac86febcc497987019fc2f1918d8
Amit Prasad <mail@amitprasad.dev>
2025-01-28 13:05:40 -0600

    Flatten libc_riscv32

--- crates/libc-riscv32/src/lib.rs
+++ crates/libc-riscv32/src/lib.rs
@@ -1,16 +1,119 @@
-mod syscalls;
-pub use syscalls::*;
+// sys/mman.h
+pub const MAP_FAILED: i32 = -1;
 
-mod errno;
-pub use errno::*;
+pub const MAP_SHARED: u32 = 0x01;
+pub const MAP_PRIVATE: u32 = 0x02;
+pub const MAP_SHARED_VALIDATE: u32 = 0x03;
+pub const MAP_TYPE: u32 = 0x0f;
+pub const MAP_FIXED: u32 = 0x10;
+pub const MAP_ANON: u32 = 0x20;
+pub const MAP_ANONYMOUS: u32 = MAP_ANON;
+pub const MAP_NORESERVE: u32 = 0x4000;
+pub const MAP_GROWSDOWN: u32 = 0x0100;
+pub const MAP_DENYWRITE: u32 = 0x0800;
+pub const MAP_EXECUTABLE: u32 = 0x1000;
+pub const MAP_LOCKED: u32 = 0x2000;
+pub const MAP_POPULATE: u32 = 0x8000;
+pub const MAP_NONBLOCK: u32 = 0x10000;
+pub const MAP_STACK: u32 = 0x20000;
+pub const MAP_HUGETLB: u32 = 0x40000;
+pub const MAP_SYNC: u32 = 0x80000;
+pub const MAP_FIXED_NOREPLACE: u32 = 0x100000;
+pub const MAP_FILE: u32 = 0;
 
-mod mman;
-pub use mman::*;
+pub const PROT_NONE: u32 = 0;
+pub const PROT_READ: u32 = 1;
+pub const PROT_WRITE: u32 = 2;
+pub const PROT_EXEC: u32 = 4;
 
-mod elf;
-pub use elf::*;
+// ELF auxiliary vector
+pub const AT_NULL: u32 = 0;
+pub const AT_IGNORE: u32 = 1;
+pub const AT_EXECFD: u32 = 2;
+pub const AT_PHDR: u32 = 3;
+pub const AT_PHENT: u32 = 4;
+pub const AT_PHNUM: u32 = 5;
+pub const AT_PAGESZ: u32 = 6;
+pub const AT_BASE: u32 = 7;
+pub const AT_FLAGS: u32 = 8;
+pub const AT_ENTRY: u32 = 9;
+pub const AT_NOTELF: u32 = 10;
+pub const AT_UID: u32 = 11;
+pub const AT_EUID: u32 = 12;
+pub const AT_GID: u32 = 13;
+pub const AT_EGID: u32 = 14;
+pub const AT_PLATFORM: u32 = 15;
+pub const AT_HWCAP: u32 = 16;
+pub const AT_CLKTCK: u32 = 17;
+pub const AT_SECURE: u32 = 23;
+pub const AT_BASE_PLATFORM: u32 = 24;
+pub const AT_RANDOM: u32 = 25;
+pub const AT_HWCAP2: u32 = 26;
+pub const AT_RSEQ_FEATURE_SIZE: u32 = 27;
+pub const AT_RSEQ_ALIGN: u32 = 28;
+pub const AT_HWCAP3: u32 = 29;
+pub const AT_HWCAP4: u32 = 30;
+pub const AT_EXECFN: u32 = 31;
 
-mod futex;
-pub use futex::*;
-
+// rlimit
 pub const RLIM_INFINITY: u32 = -1i32 as u32;
+
+// futex
+pub const FUTEX_WAIT: u32 = 0;
+pub const FUTEX_WAKE: u32 = 1;
+pub const FUTEX_FD: u32 = 2;
+pub const FUTEX_REQUEUE: u32 = 3;
+pub const FUTEX_CMP_REQUEUE: u32 = 4;
+pub const FUTEX_WAKE_OP: u32 = 5;
+pub const FUTEX_LOCK_PI: u32 = 6;
+pub const FUTEX_UNLOCK_PI: u32 = 7;
+pub const FUTEX_TRYLOCK_PI: u32 = 8;
+pub const FUTEX_WAIT_BITSET: u32 = 9;
+pub const FUTEX_WAKE_BITSET: u32 = 10;
+pub const FUTEX_WAIT_REQUEUE_PI: u32 = 11;
+pub const FUTEX_CMP_REQUEUE_PI: u32 = 12;
+pub const FUTEX_LOCK_PI2: u32 = 13;
+
+pub const FUTEX_PRIVATE_FLAG: u32 = 128;
+pub const FUTEX_CLOCK_REALTIME: u32 = 256;
+pub const FUTEX_CMD_MASK: u32 = !(FUTEX_PRIVATE_FLAG | FUTEX_CLOCK_REALTIME);
+
+pub const FUTEX_BITSET_MATCH_ANY: u32 = 0xffffffff;
+
+// errno
+pub const EPERM: i32 = 1;
+pub const ENOENT: i32 = 2;
+pub const ESRCH: i32 = 3;
+pub const EINTR: i32 = 4;
+pub const EIO: i32 = 5;
+pub const ENXIO: i32 = 6;
+pub const E2BIG: i32 = 7;
+pub const ENOEXEC: i32 = 8;
+pub const EBADF: i32 = 9;
+pub const ECHILD: i32 = 10;
+pub const EAGAIN: i32 = 11;
+pub const ENOMEM: i32 = 12;
+pub const EACCES: i32 = 13;
+pub const EFAULT: i32 = 14;
+pub const ENOTBLK: i32 = 15;
+pub const EBUSY: i32 = 16;
+pub const EEXIST: i32 = 17;
+pub const EXDEV: i32 = 18;
+pub const ENODEV: i32 = 19;
+pub const ENOTDIR: i32 = 20;
+pub const EISDIR: i32 = 21;
+pub const EINVAL: i32 = 22;
+pub const ENFILE: i32 = 23;
+pub const EMFILE: i32 = 24;
+pub const ENOTTY: i32 = 25;
+pub const ETXTBSY: i32 = 26;
+pub const EFBIG: i32 = 27;
+pub const ENOSPC: i32 = 28;
+pub const ESPIPE: i32 = 29;
+pub const EROFS: i32 = 30;
+pub const EMLINK: i32 = 31;
+pub const EPIPE: i32 = 32;
+pub const EDOM: i32 = 33;
+pub const ERANGE: i32 = 34;
+pub const EWOULDBLOCK: i32 = EAGAIN;
+pub const ENOSYS: i32 = 38;
